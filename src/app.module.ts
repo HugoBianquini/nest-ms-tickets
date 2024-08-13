9025fba9dad5f52700a6b5f65aa62c796fe51d33
HugoBianquini <hugobianqui@hotmail.com>
2024-08-12 22:55:58 -0300

    feat: create sample crud for events

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EventController } from './event/event.controller';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, EventController],
+  imports: [EventsModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}

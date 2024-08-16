ef52ecf421bb3c3576005490c59b5f888aeecf8d
HugoBianquini <hugobianqui@hotmail.com>
2024-08-16 00:19:43 -0300

    feat: setup nestjs config module to read env vars

--- apps/partner1/src/partner1.module.ts
+++ apps/partner1/src/partner1.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EventsModule } from './events/events.module';
 import { SpotsModule } from './spots/spots.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EventsModule, SpotsModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env.partner1' }),
+    EventsModule,
+    SpotsModule,
+  ],
 })
 export class Partner1Module {}
